32aa762d6ecf9763f05f6bc63455d7f40a29ce5c
Darshan26B <iamdarshan6239db@gmail.com>
2023-03-12 19:11:16 +0530

    Commit to Commit1

--- app/src/main/java/com/ca/project/SplashActivity.java
+++ app/src/main/java/com/ca/project/SplashActivity.java
@@ -15,17 +15,18 @@ public class SplashActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-       TextView R1= findViewById(R.id.R1);
-
-        Animation animation = AnimationUtils.loadAnimation(SplashActivity.this,R.anim.animation);
-        R1.startAnimation(animation);
         setContentView(R.layout.activity_splash);
+        TextView R1 = findViewById(R.id.R1);
+
+        Animation animation = AnimationUtils.loadAnimation(SplashActivity.this, R.anim.animation);
+        R1.startAnimation(animation);
+
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
-                startActivity(new Intent(SplashActivity.this,MainActivity.class));
+                startActivity(new Intent(SplashActivity.this, MainActivity.class));
                 finish();
             }
-        },3000);
+        }, 3000);
     }
 }
