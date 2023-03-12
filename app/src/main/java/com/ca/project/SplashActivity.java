b2538321799fd03a53051949409be868f16e9536
Darshan26B <iamdarshan6239db@gmail.com>
2023-03-12 18:48:46 +0530

    Commit to Commit1

---
+++ app/src/main/java/com/ca/project/SplashActivity.java
@@ -0,0 +1,31 @@
+package com.ca.project;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.TextView;
+
+public class SplashActivity extends AppCompatActivity {
+    @SuppressLint("MissingInflatedId")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+       TextView R1= findViewById(R.id.R1);
+
+        Animation animation = AnimationUtils.loadAnimation(SplashActivity.this,R.anim.animation);
+        R1.startAnimation(animation);
+        setContentView(R.layout.activity_splash);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(SplashActivity.this,MainActivity.class));
+                finish();
+            }
+        },3000);
+    }
+}
